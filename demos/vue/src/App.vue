e3c906fed01fb9a0685a70aeda21bf1cf62c41c6
lujianqing <lujiangqing@pjlab.org.cn>
2024-04-07 19:35:33 +0800

    base09

--- demos/vue/src/App.vue
+++ demos/vue/src/App.vue
@@ -1,11 +1,122 @@
 <script setup lang="ts">
+  import { onMounted, defineAsyncComponent } from 'vue';
+  import FormTest from './Form';
 
+  // import { Form } from 'form-cross-view-core';
+  // import { genCreateViewVue, genMountViewVue } from 'form-cross-view-vue';
+  // import 'form-cross-view-vue/dist/style.css';
+
+  let setRormRender: (form: any) => void;
+  const Form = defineAsyncComponent(() => {
+    return new Promise((resolve) => {
+      setRormRender = resolve;
+    });
+  });
+
+  onMounted(() => {
+    const descriptor = {
+      type: 'object',
+      required: true,
+      editable: true,
+      comment: `
+  1.小车装有超声波传感器，可检测到前方的石头；
+  2.请补充小车的控制逻辑，将石头移出圆形区域。
+      `,
+      fields: {
+        a: {
+          type: 'string',
+          required: true,
+          defaultValue: 'hello',
+          comment: 'a 的注释',
+          editable: true,
+        },
+  //       b: {
+  //         type: 'number',
+  //         required: true,
+  //         min: 1,
+  //         max: 18,
+  //         defaultValue: 1,
+  //         editable: true,
+  //       },
+  //       c: {
+  //         type: 'integer',
+  //         required: true,
+  //         min: -1,
+  //         max: 16,
+  //         defaultValue: 2,
+  //         editable: true,
+  //       },
+  //       d: {
+  //         type: 'float',
+  //         required: true,
+  //         defaultValue: 3.1,
+  //         editable: true,
+  //       },
+  //       e: {
+  //         type: 'boolean',
+  //         required: true,
+  //         defaultValue: false,
+  //         editable: true,
+  //       },
+  //       f: {
+  //         type: 'enum',
+  //         required: true,
+  //         enum: ['admin', 'user', 'guest'],
+  //         defaultValue: 'admin',
+  //         editable: true,
+  //       },
+        vertices: {
+          type: 'array',
+          editable: true,
+          comment: `
+  数组多行注释
+  数组多行注释
+          `,
+          defaultField: {
+            type: 'array',
+            len: 2,
+            editable: true,
+            defaultField: {
+              type: 'number',
+              required: true,
+              editable: true,
+              defaultValue: 0,
+            }
+          },
+          defaultValue: [[-0.5, -0.5]],
+        },
+      },
+    }
+
+    // const updateForm = async (descriptor: any, value?: any) => {
+    //   const formDiv = document.getElementById('form');
+    //   if (!formDiv) {
+    //     throw Error('missing formDiv');
+    //   }
+    //   formDiv.innerHTML = '';
+
+    //   const formInstance = new Form(
+    //     formDiv,
+    //     descriptor,
+    //     {
+    //       createView: genCreateViewVue(),
+    //       mountView: genMountViewVue(setFormRender),
+    //     }
+    //   );
+
+    //   console.log(formInstance);
+    // }
+
+    // updateForm(descriptor);
+
+    setRormRender(FormTest);
+  });
 </script>
 
 <template>
-  <div>
-    HelloWorld
-  </div>
+    <div className='formContainer'>
+      <div id='form'><Form /></div>
+    </div>
 </template>
 
 <style scoped>
--- packages/form-cross-view-vue/index.ts
+++ packages/form-cross-view-vue/index.ts
@@ -1,10 +1,11 @@
+import { ref, provide, inject } from 'vue';
 import { Form, FormNode } from 'form-cross-view-core';
 
 import stylesDefault from './index.module.scss';
 
 export type Styles = { [k: string]: any }
 
-export function genCreateViewReact(styles?: Styles) {
+export function genCreateViewVue(styles?: Styles) {
   if (!styles) {
     styles = stylesDefault;
   }
@@ -23,24 +24,37 @@ export function genCreateViewReact(styles?: Styles) {
       name: fieldName, type, comment,
     } = controller;
 
-    const Container = (props: any) => {
-      return (
-        <div className={getClass(styles, 'formField')}>{props.children}</div>
-      )
-    }
-
-    const Comment = (props: any) => {
-      return (
-        <div className={getClass(styles, 'comment')}>{comment}</div>
-      )
-    }
-
-    const Label = (props: any) => {
-      const FieldOperations = () => {
-        if (!controller.isArrayItem) {
-          return (<></>);
+    const Container = {
+      setup() {
+        return {
+          styles,
+          getClass,
         }
+      },
+      template: `
+        <div :class="{[getClass(styles, 'formField')]: true}">
+          <slot />
+        </div>
+      `,
+    }
 
+    const Comment = {
+      setup() {
+        return {
+          styles,
+          getClass,
+          comment,
+        }
+      },
+      template: `
+        <div :class="{[getClass(styles, 'comment')]: true}">
+          {{comment}}
+        </div>
+      `,
+    }
+
+    const FieldOperations = {
+      setup() {
         const onMoveUp = async () => {
           await node.onViewChange({
             source: 'operation',
@@ -69,42 +83,187 @@ export function genCreateViewReact(styles?: Styles) {
           });
         }
 
-        return (
-          <div className={getClass(styles, 'operations')}>
-            <span className={getClass(styles, 'item')} onClick={onMoveUp}>上移</span>
-            <span className={getClass(styles, 'item')} onClick={onMoveDown}>下移</span>
-            <span className={getClass(styles, 'item')} onClick={onDelete}>删除</span>
-            <span className={getClass(styles, 'item')} onClick={onCopy}>复制</span>
+        return {
+          styles,
+          getClass,
+          onMoveUp,
+          onMoveDown,
+          onDelete,
+          onCopy,
+        }
+      },
+      template: `
+        <div :class="{[getClass(styles, 'operations')]: true}">
+          <span :class="{[getClass(styles, 'item')]: true}" @click=""onMoveUp">上移</span>
+          <span :class="{[getClass(styles, 'item')]: true}" @click="onMoveDown">下移</span>
+          <span :class="{[getClass(styles, 'item')]: true}" @click="onDelete">删除</span>
+          <span :class="{[getClass(styles, 'item')]: true}" @click="onCopy">复制</span>
+        </div>
+      `,
+    }
+
+    const Label = {
+      components: {
+        FieldOperations,
+      },
+      props: ['valueVisible', 'name'],
+      setup() {
+        return {
+          styles,
+          getClass,
+          node,
+        }
+      },
+      template: (function() {
+        switch(type) {
+          case 'array':
+          case 'object': {
+            return `
+              <div :class="{[getClass(styles, 'fieldName')]: true}">
+                <span
+                  :class="{
+                    [getClass(styles, 'valueVisibleCtrl')]: true,
+                    [getClass(styles, 'fold')]: !props.valueVisible,
+                  }"}
+                  @click="() => node.valueVisible = !node.valueVisible"
+                >
+                  {{'>'}}
+                </span>
+                <span>{{props.name}}</span>
+                <FieldOperations v-if="node.isArrayItem" />
+              </div>
+            )`
+          }
+          default:
+        }
+        return `
+          <div :class="{[getClass(styles, 'fieldName')]: true}">
+            {{props.name}}
+            <FieldOperations v-if="node.isArrayItem" />
           </div>
-        )
-      }
+        `
+      })(),
+    }
+
+    const Value = (function() {
+      const value = controller.getValue();
       switch(type) {
-        case 'array':
         case 'object': {
-          return (
-            <div className={getClass(styles, 'fieldName')}>
-              <span
-                className={classnames({
-                  [getClass(styles, 'valueVisibleCtrl')]: true,
+          return {
+            props: ['valueVisible'],
+            setup() {
+              return {
+                styles,
+                getClass,
+              }
+            },
+            template: `
+              <div
+                :class="{
+                  [getClass(styles, 'fieldValue')]: true,
                   [getClass(styles, 'fold')]: !props.valueVisible,
-                })}
-                onClick={() => node.valueVisible = !node.valueVisible }
+                }"
               >
-                {'>'}
-              </span>
-              <span>{props.name}</span>
-              <FieldOperations />
-            </div>
+                <slot />
+              </div>
+            `
+          }
+        }
+        case 'array': {
+          return {
+            props: ['valueVisible'],
+            setup() {
+              const onAddItem = async () => {
+                await node.onViewChange({
+                  source: 'operation',
+                  value: 'addItem',
+                });
+              }
+
+              return {
+                styles,
+                getClass,
+                onAddItem,
+              }
+            },
+            template: `
+              <div
+                :class="{
+                  [getClass(styles, 'fieldValue')]: true,
+                  [getClass(styles, 'fold')]: !props.valueVisible,
+                }"
+              >
+                <slot />
+                <div :class="{[getClass(styles, 'operations')]: true}">
+                  <button @lick="onAddItem">+ add item</button>
+                </div>
+              </div>
+            `
+          }
+        }
+        case 'string': {
+          const [valueDisplay, setValueDisplay] = useState(value);
+          node.viewCtx.setValue = (value: string) => setValueDisplay(value);
+          const onInput = async (e) => {
+            const valueCur = String(e.target?.value);
+            setValueDisplay(valueCur);
+            await node.onViewChange({
+              source: 'input',
+              value: valueCur,
+            });
+          }
+          return (
+            <input className={getClass(styles, 'fieldValue')} type={'text'} value={valueDisplay} onInput={onInput} />
+          )
+        }
+        case 'float':
+        case 'integer':
+        case 'number': {
+          const [valueDisplay, setValueDisplay] = useState(value);
+          node.viewCtx.setValue = (value: number) => setValueDisplay(value);
+          const onInput = async (e) => {
+            let valueCur = e.target?.value;
+            setValueDisplay(valueCur);
+            if (valueCur.trim() !== '') {
+              valueCur = Number(valueCur);
+            }
+            await node.onViewChange({
+              source: 'input',
+              value: valueCur,
+            });
+          }
+          return (
+            <input className={getClass(styles, 'fieldValue')} type={'text'} value={valueDisplay} onInput={onInput} />
           )
         }
         default:
       }
+      const [valueDisplay, setValueDisplay] = useState(value);
+      node.viewCtx.setValue = (value: string) => setValueDisplay(value);
       return (
-        <div className={getClass(styles, 'fieldName')}>
-          {props.name}
-          <FieldOperations />
-        </div>
+        <div className={getClass(styles, 'fieldValue')}>{valueDisplay}</div>
       )
+    })()
+
+    const ErrorView = {
+      props: ['message'],
+      setup() {
+        return {
+          styles,
+          getClass,
+          node,
+        }
+      },
+      template: `
+        <div
+          :class="{
+            [getClass(styles, 'error')]: true,
+            [getClass(styles, 'hidden')]: !props.message,
+          }"
+        >
+          {props.message}
+        </div>
+      `
     }
 
     const Value = (props: any) => {
@@ -188,19 +347,6 @@ export function genCreateViewReact(styles?: Styles) {
       )
     }
 
-    const ErrorView = (props: any) => {
-      return (
-        <div
-          className={classnames({
-            [getClass(styles, 'error')]: true,
-            [getClass(styles, 'hidden')]: !props.message,
-          })}
-        >
-          {props.message}
-        </div>
-      )
-    }
-
     const NodeView = (props: any) => {
       const { node } = props;
       
@@ -250,7 +396,7 @@ export function genCreateViewReact(styles?: Styles) {
   }
 }
 
-export function genMountViewReact(setFormRender?: Function) {
+export function genMountViewVue(setFormRender?: Function) {
   return function mountViewReact(form: Form) {
     const rootNode = form?.rootFormFiled?.node;
     if (!rootNode) {
